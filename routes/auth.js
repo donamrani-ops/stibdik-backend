8df9e2e48a5bc5672b75ed2fbeff7bd9e6a80fb5
Donamrani <donamrani@gmail.com>
2026-06-10 15:58:06 +0100

    Add files via upload

--- routes/auth.js
+++ routes/auth.js
@@ -8,6 +8,7 @@ const { protect }    = require('../middleware/auth');
 router.post('/register',         authController.register);
 router.post('/login',            authController.login);
 router.post('/google',           authController.googleLogin);
+router.post('/facebook',         authController.facebookLogin);
 router.post('/forgot-password',  authController.forgotPassword);
 router.post('/reset-password',   authController.resetPassword);
 
